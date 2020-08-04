395253e96706bcc88f1dcb7043d85022d1f2494a
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-08-04 11:31:35 +0300

    Added MPI parallel PETSc Vec <-> Eigen conversions.

--- stan/math/prim/fun/stan_petsc_interface.hpp
+++ stan/math/prim/fun/stan_petsc_interface.hpp
@@ -15,7 +15,13 @@ namespace math {
 
 namespace petsc {
 
-void PetscVecToEigenVector(const Vec& pvec, Eigen::VectorXd& evec)
+/**
+ * Convert sequential PETSc Vec to Eigen::Vector.
+ *
+ * @param pvec Vec input vector to be transformed to Eigen
+ * @param evec Eigen::VectorXd& reference to Eigen representation of PETSc Vec
+ */
+void PetscVecToEigenVectorSeq(const Vec& pvec, Eigen::VectorXd& evec)
 {
     PetscErrorCode ierr;
     PetscScalar *pdata;
@@ -33,11 +39,69 @@ void PetscVecToEigenVector(const Vec& pvec, Eigen::VectorXd& evec)
     ierr = VecRestoreArray(pvec, &pdata);CHKERRXX(ierr);
 }
 
-Vec EigenVectorToPetscVec(const Eigen::Ref<const Eigen::VectorXd>& evec)
+/**
+ * Convert parallel PETSc Vec to Eigen::Vector.
+ * This function creates a vector and a scatter context that copies all vector values
+ * to each processor, so that each processor has identical Eigen::Vector.
+ *
+ * @param pvec Vec input vector to be transformed to Eigen
+ * @param evec Eigen::VectorXd& reference to Eigen representation of PETSc Vec
+ */
+void PetscVecToEigenVectorMPI(const Vec& pvec, Eigen::VectorXd& evec)
+{
+    PetscErrorCode ierr;
+
+    // create scatter context and sequential PETSc Vec
+    VecScatter scatter_ctx;
+    Vec pvec_seq;
+    ierr = VecScatterCreateToAll(pvec, &scatter_ctx, &pvec_seq);CHKERRXX(ierr);
+
+    // scatter as many times as you need
+    ierr = VecScatterBegin(scatter_ctx, pvec, pvec_seq, INSERT_VALUES, SCATTER_FORWARD);CHKERRXX(ierr);
+    ierr = VecScatterEnd(scatter_ctx, pvec, pvec_seq, INSERT_VALUES, SCATTER_FORWARD);CHKERRXX(ierr);
+
+    PetscVecToEigenVectorSeq(pvec_seq, evec);
+
+    // destroy scatter context and local vector when no longer needed
+    ierr = VecScatterDestroy(&scatter_ctx);CHKERRXX(ierr);
+    ierr = VecDestroy(&pvec_seq);CHKERRXX(ierr);
+}
+
+/**
+ * Convert sequential Eigen::Vector to sequential PETSc Vec.
+ *
+ * @param evec Eigen::Ref<Eigen::VectorXd> reference to Eigen Vector to be transformed into PETSc Vec
+ * @return Vec parallel PETSc Vec representation of input Eigen Vector
+ */
+Vec EigenVectorToPetscVecSeq(const Eigen::Ref<const Eigen::VectorXd>& evec)
 {
     PetscErrorCode ierr;
     Vec pvec;
     ierr = VecCreateSeqWithArray(PETSC_COMM_SELF, 1, evec.size(), evec.data(), &pvec);CHKERRXX(ierr);
+    return pvec;
+}
+
+/**
+ * Convert sequential Eigen::Vector to parallel PETSc Vec.
+ * Content of Eigen::Vector is split beetween processes using standard PETSc parallel layout.
+ *
+ * @param evec Eigen::Ref<Eigen::VectorXd> reference to Eigen Vector to be transformed into PETSc Vec
+ * @return Vec parallel PETSc Vec representation of input Eigen Vector
+ */
+Vec EigenVectorToPetscVecMPI(const Eigen::Ref<const Eigen::VectorXd>& evec)
+{
+    PetscErrorCode ierr;
+    Vec pvec;
+
+    PetscInt rank;
+    ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRXX(ierr);
+
+    PetscInt N = evec.size();  // global length
+    PetscInt n = PETSC_DECIDE;  // local length
+    PetscInt bs = 1;
+
+    ierr = PetscSplitOwnership(PETSC_COMM_WORLD, &n, &N);CHKERRXX(ierr);
+    ierr = VecCreateMPIWithArray(PETSC_COMM_WORLD, bs, n, N, evec.data()+rank*n, &pvec);CHKERRXX(ierr);
     return pvec;
 }
 
@@ -67,7 +131,7 @@ public:
         }
 
         // Convert Eigen input to PETSc Vec
-        Vec petsc_x = EigenVectorToPetscVec(x);
+        Vec petsc_x = EigenVectorToPetscVecSeq(x);
 
         // Initialize PETSc Real to hold the results
         PetscReal petsc_out;
@@ -100,7 +164,7 @@ public:
 
         // Convert Eigen input to PETSc Vec
         Vec petsc_grad;
-        Vec petsc_x = EigenVectorToPetscVec(x);
+        Vec petsc_x = EigenVectorToPetscVecSeq(x);
         PetscErrorCode ierr;
         ierr = VecDuplicate(petsc_x, &petsc_grad);CHKERRXX(ierr);
 
@@ -110,7 +174,7 @@ public:
 
         // Convert PETSc Vec to Eigen
         Eigen::VectorXd out(N_);
-        PetscVecToEigenVector(petsc_grad, out);
+        PetscVecToEigenVectorSeq(petsc_grad, out);
         ierr = VecDestroy(&petsc_grad);CHKERRXX(ierr);
 
         return std::make_tuple(out);
